76cd9e9830408ecf8ae2c50ad826ff1377f9a40a
Felix <f.frauhammer@dkfz.de>
2019-11-22 15:27:47 +0100

    added dummy docu (sinew::makeOxygen)

--- R/hello.R
+++ R/hello.R
@@ -5,8 +5,42 @@
 #   Test Package:              'Ctrl + Shift + T'
 
 
-# define scale_color_sqrt (and functions it requires):
+#' ggpower: package for power transformations, useful e.g. in ggplot
+#'
+#' The foo package provides three categories of important functions:
+#' foo, bar and baz.
+#'
+#' @section scale_color_sqrt:
+#' You can add this directly to a ggplot2 command.
+#' @section Useful functions:
+#' power_trans, power_breaks and semi_scientific_formatting can be useful in
+#' many scenarios.
+#'
+#' @docType package
+#' @name ggpower
+NULL
+
+
+
+
+#' @title FUNCTION_TITLE
+#' @description FUNCTION_DESCRIPTION
+#' @param power PARAM_DESCRIPTION
+#' @return OUTPUT_DESCRIPTION
+#' @details DETAILS
+#' @examples
+#' \dontrun{
+#' if(interactive()){
+#'  #EXAMPLE1
+#'  }
+#' }
+#' @seealso
+#'  \code{\link[scales]{trans_new}}
+#' @rdname power_trans
+#' @export
+#' @importFrom scales trans_new
 power_trans <- function(power){
+# define scale_color_sqrt (and functions it requires):
   # returns transformation object that can be used in ggplot's scale_*_continuous
   scales::trans_new(
     name = "tmp",
@@ -16,31 +50,85 @@ power_trans <- function(power){
 }
 
 
+#' @title FUNCTION_TITLE
+#' @description FUNCTION_DESCRIPTION
+#' @param lims PARAM_DESCRIPTION
+#' @param power PARAM_DESCRIPTION
+#' @param n_breaks PARAM_DESCRIPTION, Default: 5
+#' @return OUTPUT_DESCRIPTION
+#' @details DETAILS
+#' @examples
+#' \dontrun{
+#' if(interactive()){
+#'  #EXAMPLE1
+#'  }
+#' }
+#' @rdname power_breaks
+#' @export
 power_breaks <- function(lims, power, n_breaks=5){
   # Return vector of breaks that span the lims range evenly _after_ power transformation:
-  lims[1] <- max(0, lims[1]) # non-integer exponents are not defined for negative values
-  x <- seq(lims[1]^power, lims[2]^(power), length.out = n_breaks)^(1/power)
+  lims[1] <- base::max(0, lims[1]) # non-integer exponents are not defined for negative values
+  x <- base::seqseq(lims[1]^power, lims[2]^(power), length.out = n_breaks)^(1/power)
   # make human-readable by rounding to the closest integer power of 2. Smallest
   # and largest ticks are not strictly rounded - instead they are moved within
   # the range of values, since ggplot would not display them otherwise:
-  x <- case_when(
-    x == max(x) ~ 2^(floor(log2(x))),
-    x == min(x) ~ 2^(ceiling(log2(x))),
-    TRUE ~ (2^(round(log2(x))))
+  x <- dplyr::case_when(
+    x == base::max(x) ~ 2^(base::floor(base::log2(x))),
+    x == base::min(x) ~ 2^(base::ceiling(base::log2(x))),
+    TRUE ~ (2^(base::round(base::log2(x))))
   )
   return(x)
 }
+
+
+#' @title FUNCTION_TITLE
+#' @description FUNCTION_DESCRIPTION
+#' @param x PARAM_DESCRIPTION
+#' @return OUTPUT_DESCRIPTION
+#' @details DETAILS
+#' @examples
+#' \dontrun{
+#' if(interactive()){
+#'  #EXAMPLE1
+#'  }
+#' }
+#' @seealso
+#'  \code{\link[scales]{scientific_format}}
+#' @rdname semi_scientific_formatting
+#' @export
+#' @importFrom scales scientific
 semi_scientific_formatting <- function(x) {
   # takes numeric vector x and returns character vector where extremely large / small
   # numbers are in scientific notation (e.g. 1e-30) while others are untouched:
-  x <- case_when(
+  x <- dplyr::case_when(
     x == 0 ~ as.character(0),
-    abs(x) < .01 | abs(x) >= 1000 ~ scales::scientific(x,  digits = 0),
+    base::abs(x) < .01 | base::abs(x) >= 1000 ~ scales::scientific(x,  digits = 0),
     TRUE ~ as.character(x))}
 # if there are NAs in the vector by which you color, make sure to supply na.value.
 # Otherwise it defaults to dark grey, which looks like the high values of cubeHelix (green/black)
-scale_color_sqrt <- function(...){scale_color_gradientn(
+
+
+
+#' @title FUNCTION_TITLE
+#' @description FUNCTION_DESCRIPTION
+#' @param ... PARAM_DESCRIPTION
+#' @return OUTPUT_DESCRIPTION
+#' @details DETAILS
+#' @examples
+#' \dontrun{
+#' if(interactive()){
+#'  ggplot(data=mtcars, aes(wt, mpg, col = disp)) + geom_point() + scale_color_sqrt()
+#'  }
+#' }
+#' @seealso
+#'  \code{\link[ggplot2]{scale_colour_gradient}}
+#'  \code{\link[rje]{cubeHelix}}
+#' @rdname scale_color_sqrt
+#' @export
+#' @importFrom ggplot2 scale_color_gradientn
+#' @importFrom rje cubeHelix
+scale_color_sqrt <- function(...){ggplot2::scale_color_gradientn(
   colours = rev(rje::cubeHelix(100))[5:100],
-  trans = power_trans(1/2),
-  labels = semi_scientific_formatting,
+  trans = ggpower::power_trans(1/2),
+  labels = ggpower::semi_scientific_formatting,
   ...)}
